f35bb431056a0fae7301bbc6534a1205acfd85ab
Clark Chen <schen237@uic.edu>
2018-10-18 04:25:42 +0000

    upload

---
+++ hello-cpp-world.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Clark Chen: schen237" << endl;
+    cout << "Late Registered: noPartner" << endl;
+    cout << "Chocolate Ice Cream" << endl;
+}
